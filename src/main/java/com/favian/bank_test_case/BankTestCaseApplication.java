2d2692eacb3262de56ce534b135c0ad4f92312f0
favian47 <dpastuhov1967@gmail.com>
2026-02-12 15:55:41 +0200

    init commit

---
+++ src/main/java/com/favian/bank_test_case/BankTestCaseApplication.java
@@ -0,0 +1,13 @@
+package com.favian.bank_test_case;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTestCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankTestCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/favian/bank_test_case/BankTestCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.favian.bank_test_case;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTestCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
