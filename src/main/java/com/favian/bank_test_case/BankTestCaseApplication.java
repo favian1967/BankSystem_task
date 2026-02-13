e3a59ef0d21cbe5d885812e28ed7b7374a011534
favian47 <dpastuhov1967@gmail.com>
2026-02-13 17:32:34 +0200

    DB: add migration

--- src/main/java/com/favian/bank_test_case/BankTestCaseApplication.java
+++ src/main/java/com/favian/bank_test_case/BankTestCaseApplication.java
@@ -1,6 +1,5 @@
 package com.favian.bank_test_case;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,15 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BankTestCaseApplication {
 
     public static void main(String[] args) {
-
-        Dotenv dotenv = Dotenv.configure()
-                .ignoreIfMissing()
-                .load();
-
-        dotenv.entries().forEach(entry ->
-                System.setProperty(entry.getKey(), entry.getValue())
-        );
-
         SpringApplication.run(BankTestCaseApplication.class, args);
     }
 }
