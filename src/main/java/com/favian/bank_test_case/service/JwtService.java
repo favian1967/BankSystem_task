dc2800a824da991dbf09b94099e9636d63e4e8e1
favian47 <dpastuhov1967@gmail.com>
2026-02-14 14:10:26 +0200

    CI: defaul value in yml

--- src/main/java/com/favian/bank_test_case/service/JwtService.java
+++ src/main/java/com/favian/bank_test_case/service/JwtService.java
@@ -16,10 +16,10 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    @Value("${jwt.secret:dGVzdF9zZWNyZXRfa2V5X2Zvcl9qd3RfdG9rZW5fZ2VuZXJhdGlvbl8zMl9ieXRlcw==}")
+    @Value("${jwt.secret}")
     private String secret;
 
-    @Value("${jwt.access-expiration:3600000}")
+    @Value("${jwt.access-expiration}")
     private long accessExpiration;
 
     public String generateAccessToken(UserDetails userDetails) {
